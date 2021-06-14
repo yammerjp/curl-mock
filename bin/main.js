0dc07985b4135a0ad3223a69e23cf64b13663adc
Keisuke Nakayama <me@yammer.jp>
2021-06-14 23:04:02 +0900

    hello, world with http

--- bin/main.js
+++ bin/main.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('hello, world');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log(`request ${request.url}`);
+
+  const status = 200;
+  const header = {
+    'Content-Type': 'text/plain'
+  };
+  const resBody = 'hello, world!';
+  response.writeHead(status, header);
+  response.end(resBody, 'utf-8');
+})
+
+const port = 3333
+server.listen(port);
+console.log(`A server of curl-mock is running at http://localhost:${port}/`);
