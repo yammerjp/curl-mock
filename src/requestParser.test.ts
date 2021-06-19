a9579d2f148743481b6fe687f24f8119857b7d73
Keisuke Nakayama <me@yammer.jp>
2021-06-19 12:34:06 +0900

    Fix: test

--- src/requestParser.test.ts
+++ src/requestParser.test.ts
@@ -37,5 +37,6 @@ test('requestParser', () => {
     header: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
     },
+    method: 'GET'
   })
 })
