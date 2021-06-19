a9f74fe2174c409cd296978f953d6783c82f6bfd
Keisuke Nakayama <me@yammer.jp>
2021-06-19 11:26:36 +0900

    Update: eslint, prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,48 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
-    'prettier',
+    "plugin:import/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "airbnb-base",
+    "plugin:jest/all",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
+    "no-continue": "off",
+    "no-restricted-syntax": "off",
+    "no-console": "off",
+    "no-use-before-define": "off",
+
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: { project: "./" },
+    },
   },
 };
