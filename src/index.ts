bbd1810ca070ddac0c6361c0f40a10ec25097aef
Keisuke Nakayama <me@yammer.jp>
2021-06-19 16:02:48 +0900

    Add: read commandline arguments

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,27 @@ import readFile from './readFile'
 import createEndpointFromBlocks from './createEndpoint'
 
 async function main() {
-  const documentPath = 'example/helloworld.md'
-  const port = 3000
+  let port = 3000
+  const documentPaths: string[] = []
+  const args = process.argv.slice(2)
+  for (let i = 0; i < args.length; i+=1) {
+    if ((args[i] === '-p' || args[i] === '--port')) {
+      if (i+1 >= args.length) {
+        console.error({error: 'Failed to read command-line arguments'})
+        break
+      }
+      i+=1
+      port = Number(args[i])
+      continue
+    }
+    documentPaths.push(args[i])
+  }
+  if (documentPaths.length === 0) {
+    console.error({error: 'Please specify any markdown files'})
+    return
+  }
 
-  const blocks = await readFile(documentPath)
+  const blocks = (await Promise.all(documentPaths.map(readFile))).flat()
   const endpoints = createEndpointFromBlocks(blocks)
   mockServer(endpoints, port)
 }
