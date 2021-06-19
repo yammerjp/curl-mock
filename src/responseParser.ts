e66359574b3a128af14658c4b539d2a06d81daa5
Keisuke Nakayama <me@yammer.jp>
2021-06-19 13:42:16 +0900

    Update: simplify

--- src/responseParser.ts
+++ src/responseParser.ts
@@ -1,11 +1,12 @@
 export default function responseParser(body: string, type: ResponseType): Response | undefined {
-  if (type !== 'response') {
-    console.error({ error: 'unknown response type', value: type })
-    return undefined
+  if (type === 'response') {
+    return {
+      status: 200,
+      header: { 'Content-Type': 'text/plain' },
+      body
+    }
   }
-  return {
-    status: 200,
-    header: { 'Content-Type': 'text/plain' },
-    body
-  }
+  console.error({ error: 'unknown response type', value: type })
+  return undefined
+
 }
