405c9525d3e024872f9c684fdf084c76a5cd3763
Keisuke Nakayama <me@yammer.jp>
2021-06-24 00:02:01 +0900

    Update: remove webpack

--- bin/curldoc.js
+++ bin/curldoc.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-require('../dist/main.js');
+/* eslint-disable import/extensions */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const curldoc = require('../dist/index.js');
+
+const parseProcessArgv = () => {
+  let port = 3000
+  const documentPaths = []
+  const args = process.argv.slice(2)
+  for (let i = 0; i < args.length; i += 1) {
+    if (args[i] === '-p' || args[i] === '--port') {
+      if (i + 1 >= args.length) {
+        console.error({ error: 'Failed to read command-line arguments' })
+        break
+      }
+      i += 1
+      port = Number(args[i])
+      continue
+    }
+    documentPaths.push(args[i])
+  }
+  if (documentPaths.length === 0) {
+    console.error('Usage: curldoc (--port <TCP port>) <markdown file paths ... >')
+    process.exit(1)
+  }
+  return {port, documentPaths}
+}
+
+
+const { port, documentPaths } = parseProcessArgv()
+curldoc.server(port, documentPaths)
+
--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,19 @@ import { mockServer } from './mockServer'
 import readFile from './readFile'
 import createEndpointFromBlocks from './createEndpoint'
 
-async function main() {
-  let port = 3000
-  const documentPaths: string[] = []
-  const args = process.argv.slice(2)
-  for (let i = 0; i < args.length; i += 1) {
-    if (args[i] === '-p' || args[i] === '--port') {
-      if (i + 1 >= args.length) {
-        console.error({ error: 'Failed to read command-line arguments' })
-        break
-      }
-      i += 1
-      port = Number(args[i])
-      continue
-    }
-    documentPaths.push(args[i])
-  }
-  if (documentPaths.length === 0) {
-    console.error('Usage: curldoc <markdown file path> (--port <TCP port)')
-    return
-  }
-
+async function createEndpoints(documentPaths: string[]): Promise<Endpoint[]> {
   const blocks = (await Promise.all(documentPaths.map(readFile))).flat()
-  const endpoints = createEndpointFromBlocks(blocks)
-  mockServer(endpoints, port)
+  return createEndpointFromBlocks(blocks)
 }
 
-main()
+async function server(port: number, documentPaths: string[]): Promise<boolean> {
+  const endpoints = await createEndpoints(documentPaths)
+  try {
+    mockServer(endpoints, port)
+    return true
+  } catch {
+    return false
+  }
+}
+
+export { server, createEndpoints }
